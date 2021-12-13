9dce0b2184300381462c55dc962740f8d64a5347
Sagar P Shah <37670783+sagarshah97@users.noreply.github.com>
2021-12-13 17:37:55 +0530

    Add files via upload
    Updated GitHub link

--- force-app/main/default/lwc/contactMe/contactMe.js
+++ force-app/main/default/lwc/contactMe/contactMe.js
@@ -2,6 +2,6 @@ import { LightningElement } from "lwc";
 
 export default class ContactMe extends LightningElement {
   openGitHub() {
-    window.open("https://github.com", "_self");
+    window.open("https://github.com/sagarshah97/sfdx-project", "_self");
   }
 }
